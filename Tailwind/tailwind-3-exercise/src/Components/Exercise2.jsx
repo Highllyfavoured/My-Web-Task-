7cad8167aa57993fc825ccf089cbb015717949ea
Favour <favourolaosebikanf>
2025-11-13 12:03:05 +0100

    tailwind3

---
+++ Tailwind/tailwind-3-exercise/src/Components/Exercise2.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+
+
+const Exercise2 = () => {
+
+  return (
+    <div> 
+    {/* Navigation Links */}
+        <p className="mt-8 font-bold text-lg text-gray-800">
+          Click Here to Navigate to Other Exercises:
+        </p>
+        <div className="flex flex-wrap gap-2 mt-4">
+          <Link
+            to="/"
+            className="bg-[#333] hover:bg-[#333]/70 active:bg-[#333]/80 text-white font-bold py-2 px-4 rounded"
+          >
+            Home
+          </Link>
+          <Link
+            to="/exercise1"
+            className="bg-[#333] hover:bg-[#333]/70 active:bg-[#333]/80 text-white font-bold py-2 px-4 rounded"
+          >
+            Exercise 1
+          </Link>
+          <Link
+            to="/exercise3"
+            className="bg-[#333] hover:bg-[#333]/70 active:bg-[#333]/80 text-white font-bold py-2 px-4 rounded"
+          >
+            Exercise 3
+          </Link>
+          <Link
+            to="/exercise4"
+            className="bg-[#333] hover:bg-[#333]/70 active:bg-[#333]/80 text-white font-bold py-2 px-4 rounded"
+          >
+            Exercise 4
+          </Link>
+        </div>
+      </div>
+  );
+};
+
+export default Exercise2;
